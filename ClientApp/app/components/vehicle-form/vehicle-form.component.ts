0df172ab82381d6ba71fcf1ad1c47087044262d5
sstrazan <slaven.strazanac@farmeron.com>
2017-05-10 16:10:27 +0200

    Features checbox list implemented 2

--- ClientApp/app/components/vehicle-form/vehicle-form.component.ts
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,3 +1,4 @@
+import { FeatureService } from './../../services/feature.service';
 import { MakeService } from './../../services/make.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,14 +10,19 @@ import { Component, OnInit } from '@angular/core';
 export class VehicleFormComponent implements OnInit {
   makes: any[];
   models: any[];
+  features: any[];
   vehicle: any = {};
-  constructor(private makeService: MakeService) { }
+
+  constructor(
+    private makeService: MakeService,
+    private featureService: FeatureService) { }
 
   ngOnInit() {
     this.makeService.getMakes().subscribe(makes => {
       this.makes = makes;
       console.log("MAKES:", this.makes);
     });
+    this.featureService.getFeatures().subscribe(featues => this.features = featues);
   }
 
   onMakeChange(){
